631b0e2a479ceb8c65ad030d1d2d3a0d7493de3e
abelmorad <abelmorad97@gmail.com>
2023-05-15 23:04:17 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
 import Rate from './components/Rate'
-import Rated from './components/Rated'
 
-
-function App() {
+export default function App() {
 
   return (
     <main className="App">
-      <Routes>
-        <Route
-          path='/'
-          element={<Rate />}
-        />
-        <Route
-          path='rated'
-          element={<Rated />}
-        />
-      </Routes>
+        <Rate />
     </main>
   )
 }
-export default App
+
 
 
--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import Star from '../images/icon-star.svg'
-import Rated from './Rated'
+import ThankYou from "../images/illustration-thank-you.svg"
 
 export default function Rate() {
-  
+  const [submitted, setSubmitted] = useState(false)
     return (
-        <main className="App">
-          <section className="rating-container">
+        <>
+          {!submitted && (<section className="rating-container">
               <div className="rating-wrapper">
                 <div className="star-container">
                   <img className="star-icon" src={Star} alt="star"/>
@@ -26,27 +26,43 @@ export default function Rate() {
                 </div>
                 <SubmitButton />            
               </div>
-          </section>
-        </main>
+          </section>)}
+          {submitted && <Rated />}
+        </>
     )
 }
 
 function SubmitButton() {
-
   return (
       <button
         className="submit-btn">
           SUBMIT
       </button>  
-  )
-      
+  )     
 }
 
-function ScaleButton({scale}) {
+function ScaleButton({ scale }) {
+  return (
+      <button
+        onClick={() => (scale)}>
+          {scale}
+      </button> 
+  )
+}
 
-return (
-      <button>
-        {scale}
-      </button>
-)
+
+function Rated() {
+  return (
+      <section className="rating-container">
+          <div className="rated-wrapper">
+              <img className="ty-img" src={ThankYou} alt="thankyouart"/>
+              <p className="selected">You selected out of 5</p>
+              <h1>Thank you!</h1>
+              <p className="ty-msg">
+                  We appreciate you taking the time to give a rating.
+                  If you ever need more support, dont hesitate to get in touch!
+              </p>
+          </div>
+      </section>
+  )
 }
